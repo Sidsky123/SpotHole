907674bbe52dad37602166cb1e84d538481c643f
Sidsky123 <siddhantsiddhant6@gmail.com>
2023-04-15 01:42:17 -0400

    added db and camera

--- app/src/main/java/com/example/spothole/MainActivity.kt
+++ app/src/main/java/com/example/spothole/MainActivity.kt
@@ -1,9 +1,12 @@
 package com.example.spothole
 
 import android.Manifest
+import android.annotation.SuppressLint
+import android.content.ContentValues.TAG
 import android.content.Context
 import android.content.Intent
 import android.content.pm.PackageManager
+import android.graphics.Bitmap
 import android.hardware.Sensor
 import android.hardware.SensorEvent
 import android.hardware.SensorEventListener
@@ -14,23 +17,55 @@ import android.location.LocationManager
 import android.media.Ringtone
 import android.media.RingtoneManager
 import android.net.Uri
+import android.os.Build
 import android.os.Bundle
 import android.os.Handler
+import android.provider.MediaStore
 import android.util.Log
 import android.widget.Button
+import android.widget.ImageView
+import android.widget.Toast
+import androidx.activity.result.ActivityResultLauncher
 import androidx.activity.result.contract.ActivityResultContracts
+import androidx.activity.result.contract.ActivityResultContracts.GetContent
+import androidx.annotation.RequiresApi
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.app.ActivityCompat
 import androidx.core.content.ContextCompat
 import com.example.spothole.mqtt.MqttClientHelper
 import com.google.android.gms.maps.model.LatLng
+import com.google.android.gms.tasks.Task
 import com.google.android.material.snackbar.Snackbar
 import org.eclipse.paho.client.mqttv3.IMqttDeliveryToken
 import org.eclipse.paho.client.mqttv3.MqttCallbackExtended
 import org.eclipse.paho.client.mqttv3.MqttMessage
+import com.google.firebase.firestore.FirebaseFirestore
+import com.google.firebase.storage.FirebaseStorage
+import java.io.ByteArrayOutputStream
+import java.net.URI
+import java.time.LocalTime
+import java.util.*
+import kotlin.collections.ArrayList
 
 
+@Suppress("DEPRECATION")
 class MainActivity : AppCompatActivity() {
+    private var PICK_IMAGE_REQUEST:Int?=1
+    private var imageUri:Uri?=null
+//
+    private lateinit var captureButton: Button
+
+
+    private lateinit var storage: FirebaseStorage
+    private lateinit var firestore: FirebaseFirestore
+
+    private val REQUEST_IMAGE_CAPTURE = 1
+    private val CAMERA_PERMISSION_CODE = 2
+//
+    //    private var ResultLauncher
+    private lateinit var cameraintent: ActivityResultLauncher<Intent>
+    private lateinit var imageView: ImageView
+
     private lateinit var sensorManager: SensorManager
     private var sensor : Sensor ?= null
     private val mqttClient by lazy {
@@ -55,6 +90,7 @@ class MainActivity : AppCompatActivity() {
     }
 
     private val mListener: SensorEventListener = object : SensorEventListener {
+        @RequiresApi(Build.VERSION_CODES.O)
         override fun onSensorChanged(event: SensorEvent) {
             if (event.values[0] - 9.8 > 8) {
             Log.d("MY_APP", event.values[1].toString())
@@ -64,11 +100,23 @@ class MainActivity : AppCompatActivity() {
                         "spothole",
                         currloc?.latitude.toString() + " , " + currloc?.longitude.toString()
                     )
-                    Log.d("LOC", currloc?.latitude.toString() + " , " + currloc?.longitude.toString())
+                    Log.d("LOC12345", currloc?.latitude.toString() + " , " + currloc?.longitude.toString())
                 }
                 Handler().postDelayed({
                     sensorManager.registerListener(this, sensor, SensorManager.SENSOR_DELAY_NORMAL)
                 }, 2000)
+
+                val db = FirebaseFirestore.getInstance()
+                //store code in firebase
+                val user: MutableMap<String, Any> = HashMap()
+                user["Longitude"] = currloc?.longitude.toString()
+                user["Latitude"] = currloc?.latitude.toString()
+                user["Date"] = LocalTime.now()
+
+                db.collection("PotHole")
+                    .add(user)
+                    .addOnSuccessListener { documentReference -> Log.d("Message", "DocumentSnapshot added with ID: " + documentReference.id) }
+                    .addOnFailureListener { e -> Log.w("Message", "Error adding document",e)}
             }
         }
 
@@ -96,6 +144,7 @@ class MainActivity : AppCompatActivity() {
                 val coord = LatLng(mqttMessage.toString().split(',')[0].toDouble(), mqttMessage.toString().split(',')[1].toDouble());
                 coordinates.add(coord)
                 println("Coordinates added to the list: $coordinates")
+                Log.d(TAG, "messageArrived: $coordinates")
 
             }
 
@@ -145,11 +194,16 @@ class MainActivity : AppCompatActivity() {
         }
     }
 
+    @SuppressLint("MissingInflatedId")
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
+        //imageView=findViewById(R.id.image_view2)
         setContentView(R.layout.activity_main)
         setMqttCallBack()
         checkForPermissions()
+//        cameraintent=registerForActivityResult(){result->
+//
+//        }
 
         var listPotholes: Button = findViewById(R.id.list_potholes)
         listPotholes.setOnClickListener{
@@ -179,6 +233,21 @@ class MainActivity : AppCompatActivity() {
                     .setAction("Action", null).show()
             }
 
+        }
+        cameraintent = registerForActivityResult(ActivityResultContracts.StartActivityForResult()){result->
+            if(result.resultCode== RESULT_OK)
+            {
+                val data=result.data!!.getStringExtra("data")
+                Toast.makeText(this,data,Toast.LENGTH_SHORT).show()
+            }
+        }
+        captureButton= findViewById(R.id.upload_img)
+
+        captureButton.setOnClickListener(){
+            val intent= Intent(this,CaptureImage::class.java)
+            intent.putExtra("coordinates", coordinates)
+            startActivity(intent)
+           //cameraintent.launch(Intent("android.media.action.IMAGE_CAPTURE"))
         }
     }
 }
